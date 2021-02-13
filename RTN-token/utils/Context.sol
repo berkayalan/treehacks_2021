9c683966e67a65b199537d42841a0363bac1b857
Vaani Rawat <53212353+vaanicodes@users.noreply.github.com>
2021-02-13 13:30:47 +0530

    Create Context.sol

---
+++ RTN-token/utils/Context.sol
@@ -0,0 +1,14 @@
+
+
+pragma solidity ^0.8.0;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        this; 
+        return msg.data;
+    }
+}
